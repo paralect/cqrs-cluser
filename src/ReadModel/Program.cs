e93d1c8b57746d7228c32e08e7918a9ac812d59a
Alexander Yarmolovich <a.yarmolovich@paralect.com>
2017-04-04 17:14:57 +0300

    read messages from ReadModelQueue

--- src/ReadModel/Program.cs
+++ src/ReadModel/Program.cs
@@ -1,9 +1,60 @@
 ﻿namespace ReadModel
 {
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using ParalectEventSourcing.Messaging.RabbitMq;
+    using ParalectEventSourcing.Serialization;
+    using RabbitMQ.Client.Events;
+
     public class Program
     {
+        private static IServiceProvider _serviceProvider;
+
         public static void Main(string[] args)
         {
+            RegisterDependencies();
+            ListenToMessages();
+
+            Console.Read();
+        }
+
+        private static void RegisterDependencies()
+        {
+            var rabbitMqConnectionSettings = new RabbitMqConnectionSettings
+            {
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "/",
+                HostName = "localhost",
+                Port = 5672
+            }; // TODO read from configuration
+
+            _serviceProvider = new ServiceCollection()
+
+                // TODO consider creating channels per thread
+                .AddTransient<IChannel, Channel>()
+                .AddSingleton<RabbitMqConnectionSettings>(rabbitMqConnectionSettings)
+                .AddSingleton<IChannelFactory, ChannelFactory>()
+                .AddTransient<IMessageSerializer, DefaultMessageSerializer>()
+
+                .BuildServiceProvider();
+        }
+
+        private static void ListenToMessages()
+        {
+            Task.Run(() =>
+            {
+                var channel = _serviceProvider.GetService<IChannel>();
+                channel.Listen(QueueConfiguration.ReadModelQueue, ConsumerOnReceived);
+            });
+        }
+
+        private static void ConsumerOnReceived(object sender, BasicDeliverEventArgs basicDeliverEventArgs)
+        {
+            var body = Encoding.UTF8.GetString(basicDeliverEventArgs.Body);
+            Console.WriteLine("Received: " + body);
         }
     }
 }
