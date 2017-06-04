57de4741ce0d84e0aefddc14be964b31c8b75d7b
Alexander Yarmolovich <yarmolovichalex@gmail.com>
2017-06-04 16:19:13 +0300

    rabbitmq - configure persistence in docker-compose.yml

--- docker_remove_all.sh
+++ docker_remove_all.sh
@@ -1,3 +1,5 @@
+# Remove all CQRS cluster containers with related volumes; Frontend, Webapi, WriteModel, ReadModel images; dangling volumes  
+
 docker rm -f -v $(docker ps -a -q)
 docker rmi -f $(docker images | grep 'frontend\|webapi\|writemodel\|readmodel' | awk '{ print $3 }' )
 docker volume rm $(docker volume ls -q -f dangling=true)
